236c7a34c703e180604f1d0661c0ae2fe623eedc
IssyYeung <issyyeung@hotmail.co.uk>
2021-04-30 21:37:07 +0100

    Fixed heading font

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,12 +1,33 @@
 import { NavLink } from "react-router-dom";
+import style from "./navbar.module.scss";
+import stats from "../../images/stats.png";
+import computer from "../../images/computer.png";
+import tama from "../../images/tama.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <NavLink to="/play">Play</NavLink>
-      <NavLink to="/stats">Stats</NavLink>
-      <NavLink to="/account">Account</NavLink>
-    </div>
+    <nav className={style.navbar}>
+      <div>
+        {/* <span> */}
+        <NavLink to="/play">
+          <img src={tama} alt="Tama pixel art" />
+          Play
+        </NavLink>
+        {/* </span>
+        <span> */}
+        <NavLink to="/stats">
+          <img src={stats} alt="Graph pixel art" />
+          Stats
+        </NavLink>
+        {/* </span>
+        <span> */}
+        <NavLink to="/account">
+          <img src={computer} alt="Computer pixel art" />
+          Account
+        </NavLink>
+        {/* </span> */}
+      </div>
+    </nav>
   );
 };
 
