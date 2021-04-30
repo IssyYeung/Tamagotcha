6e5c109d37aaca383e55d1a9116cad19377a7310
IssyYeung <issyyeung@hotmail.co.uk>
2021-04-30 16:46:42 +0100

    login, register and account pages

--- frontend/src/pages/AccountPage.js
+++ frontend/src/pages/AccountPage.js
@@ -1,10 +1,15 @@
 import Layout from "../components/layout/Layout";
+import Button from "../components/button/Button";
+import { NavLink } from "react-router-dom";
 
 const AccountPage = () => {
   return (
     <div>
       <Layout pageTitle="TAMACCOUNT">
         <h1>Account Page</h1>
+        <NavLink to="/">
+          <Button>Logout</Button>
+        </NavLink>
       </Layout>
     </div>
   );
