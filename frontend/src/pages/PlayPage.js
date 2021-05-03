df933e0aa2896e8449d666d9688a7db5b93f74af
IssyYeung <issyyeung@hotmail.co.uk>
2021-05-03 12:11:32 +0100

    Tamagotchi background and working buttons added

--- frontend/src/pages/PlayPage.js
+++ frontend/src/pages/PlayPage.js
@@ -1,16 +1,28 @@
 import Layout from "../components/layout/Layout";
 import BottomNav from "../components/bottom_nav/BottomNav";
+import tamagotchi from "../images/tamagotchi.png";
+import style from "../styles/pageStyles/playpage.module.scss";
+import Button from "../components/button/Button";
+import { useState } from "react";
+import Creature from "../components/creature/Creature";
 
 const PlayPage = () => {
+  const [color, setColor] = useState("yellow");
   return (
-    <div>
-      <Layout pageTitle="Tamagotcha">
-        <h1>Play Page</h1>
-        {/* <HealthBar/>
-        <Tamagotcha/>*/}
-        <BottomNav />
-      </Layout>
-    </div>
+    <Layout pageTitle="Tamagotcha">
+      <div className={style.playPage}>
+        <div className={style.imageContainer}>
+          {/* <HealthBar/>*/}
+          <img src={tamagotchi} />
+          {/* <div className={style.box} style={{ backgroundColor: color }}></div> */}
+          <Creature />
+          <Button className={style.btn1} onClick={() => setColor("blue")} />
+          <Button className={style.btn2} onClick={() => setColor("red")} />
+          <Button className={style.btn3} onClick={() => setColor("yellow")} />
+        </div>
+      </div>
+      <BottomNav />
+    </Layout>
   );
 };
 
