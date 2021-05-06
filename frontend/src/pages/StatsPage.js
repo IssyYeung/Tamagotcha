98670671b7f4419374fc17c0612676dd56041b85
John Herman <64842415+JA-H@users.noreply.github.com>
2021-05-06 19:49:20 +0100

    Needs bar changes depending on lowest stat

--- frontend/src/pages/StatsPage.js
+++ frontend/src/pages/StatsPage.js
@@ -10,6 +10,48 @@ const StatsPage = () => {
   // useContext looks up component tree to find StatsContext
 
   // useEffect(() => {});
+  function indexOfMin(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var min = arr[0];
+    var minIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            minIndex = i;
+            min = arr[i];
+        }
+    }
+
+    return minIndex;
+  }
+
+  const shownStats = [state.sleep, state.thirst, state.hunger, state.fun ]
+
+  const findNeed = (array) => {
+    
+    const currentMin = indexOfMin(array)
+
+    switch( currentMin ){
+      case 0:
+        return state.name + " is sleepy!"
+      case 1:
+        return state.name + " is thirsty!"
+      case 2:
+        return state.name +  " is hungry!"
+      case 3:
+        return state.name + " is bored!"
+      default:
+        return "All stats need to be increased!"
+    }
+
+  }
+
+  useEffect(() => {
+    findNeed(shownStats)
+  }, [shownStats]);
 
   const timeBornMs = new Date(state.timeBorn);
   const age = Math.round((currentTime - timeBornMs) / (1000 * 60 * 60));
@@ -22,7 +64,7 @@ const StatsPage = () => {
             <h4>Name: {state.name}</h4>
             <h4>Breed: {state.breed}</h4>
             <h4>Age: {age}</h4>
-            <h4>Needs:</h4>
+            <h4>Needs: { findNeed(shownStats) }</h4>
           </div>
           <StatBar statTitle="Average Health" statValue={state.avgHealth} />
           <StatBar statTitle="Sleep" statValue={state.sleep} />
