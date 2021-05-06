f0677162f932d491b3f8089605e04d1b8a419cf0
Ed <edwardmarkdixon.gmail.com>
2021-05-06 12:46:22 +0100

    Disabled emotes when asleep

--- frontend/src/pages/PlayPage.js
+++ frontend/src/pages/PlayPage.js
@@ -93,9 +93,9 @@ const PlayPage = () => {
             crackState={crackState}
           />
           <Egg onClick={crackEgg} crackState={crackState} />
-          <Button className={style.btn1} onClick={handleBtn1} />
-          <Button className={style.btn2} onClick={handleBtn2} />
-          <Button className={style.btn3} onClick={handleBtn3} />
+          {isAwake && <Button className={style.btn1} onClick={handleBtn1} />}
+          {isAwake && <Button className={style.btn2} onClick={handleBtn2} />}
+          {isAwake && <Button className={style.btn3} onClick={handleBtn3} />}
         </div>
       </div>
       {isAwake && (
